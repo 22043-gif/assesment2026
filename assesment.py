bf2675c7819a03ce77d32f11f7a9c864b28fd8b8
22043-gif <22043@rangiorahigh.school.nz>
2026-09-08 10:55:46 +1200

    creating object from input

--- assesment.py
+++ assesment.py
@@ -8,9 +8,15 @@
 #must add file
 
 #Constants
+from unicodedata import name
+
+
 BASIC_KIT = 75000
 TRADE_DISCOUNT_RATE = 0.1
 GST = 0.15
+
+#Dictoinaries
+people = []
 
 class Person:
     def __init__(self, name, phone, address, discount):
@@ -19,10 +25,10 @@ class Person:
         self.address = address
         self.discount = discount
 
-    def __str__(self):
-                return '{}, {}, {}, {}'.format(self.name, self.phone, self.address, self.discount)
+    def introduce(self):
+                print(f"Hi, my name is {self.name}.")
+                print(f"My phone number is {self.phone}.")
 
-    
 def user_details():
         """
         Finds names
@@ -32,35 +38,25 @@ def user_details():
         phone = input("Phone number: ") 
         address = input("Address: ")
 
-        applied_for_discount = False
-        while not applied_for_discount:
-            trade_member = input("Are you a trade member: ")
-            if trade_member.lower() == ("yes"):
-                discount = True
-                applied_for_discount = True
-            elif trade_member.lower() == ("no"):
-                discount = False
-                applied_for_discount = True
-        
+        applied__loop = True
+        while applied__loop:
+            trade_member = input("Are you a trade member (yes/no): ")
+            if trade_member == "yes":
+                print("You are eligible for a discount")
+                applied__loop = False
+            elif trade_member == "no":
+                print("You are not eligible for a discount")
+                applied__loop = False
+            else:
+                print("Please enter yes or no")
 
-
-        
+        new_person = Person(name, phone, address, trade_member)
+        new_person.introduce()
+'''
 def build_details():
       """"
       This function will get user input on the details of what they want in their design
       """
-      bathroom_option = False
-      while not bathroom_option:
-        bathroom = input("Are you a trade member: ")
-        if trade_member.lower() == ("yes"):
-            discount = True
-            applied_for_discount = True
-        elif trade_member.lower() == ("no"):
-            discount = False
-            applied_for_discount = True
-
-
-
 
 def calculations():
       """"
@@ -68,18 +64,6 @@ def calculations():
       """
       build_details()
 
+'''
 
-
-def add_a_person():
-      """"
-      This function will use details from previos functions to add a new person as an object in the Person class
-      """
-      user_details()
-      calculations()
-
-      return Person(name, phone, address, discount)
-
-
-
-
-new_quote = user_details()
+user_details()
